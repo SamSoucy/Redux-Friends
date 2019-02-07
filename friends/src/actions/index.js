bf642d83bdd6680464960e2f5b0644d647b1b61b
Samsoucy <newf930@outlook.com>
2019-02-07 16:53:44 -0500

    added action creator

---
+++ friends/src/actions/index.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const FETCH_FRIEND_START = "FETCH_FRIEND_START";
+export const FETCH_FRIEND_SUCCESS = "FETCH_FRIEND_SUCCESS";
+export const FETCH_FRIEND_FAILURE = "FETCH_FRIEND_FAILURE";
+
+export const getFriends = () => dispatch => {
+    dispatch({ type: FETCH_FRIEND_START });
+    axios
+        .get("http://localhost:5000/api/friends")
+        .then(res => dispatch({ type: FETCH_FRIEND_SUCCESS, payload: res.data }))
+        .catch(err => dispatch({ type: FETCH_FRIEND_FAILURE, payload: err }));
+};
