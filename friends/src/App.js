5f8e07f42b15c7b81f2c4c4c86ed3543cec47537
Samsoucy <newf930@outlook.com>
2019-02-07 22:48:37 -0500

    changed some wording

--- friends/src/App.js
+++ friends/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Friends</h1>
+        <h1>Friends List!!</h1>
         <FriendForm />
         <FriendsList friends={this.props.friends} />
 
